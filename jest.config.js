2c8361efe26b2170adb270ef54295ba3962f78c3
AjitsinghKamal <ajitskamal@gmail.com>
2021-06-18 20:46:12 +0530

    chore: add linting support

--- jest.config.js
+++ jest.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-  preset: "ts-jest",
-  testEnvironment: "jsdom",
-  moduleNameMapper: {
-    "^.+\\.(css|scss|png|jpg|svg)$": "identity-obj-proxy",
-  },
-  setupFilesAfterEnv: ["<rootDir>/setupTests.ts"],
-  resetMocks: true,
+	preset: 'ts-jest',
+	testEnvironment: 'jsdom',
+	moduleNameMapper: {
+		'^.+\\.(css|scss|png|jpg|svg)$': 'identity-obj-proxy'
+	},
+	setupFilesAfterEnv: ['<rootDir>/setupTests.ts'],
+	resetMocks: true
 };
--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
-import { render, screen } from "@testing-library/react";
-import App from "./App";
+import {render, screen} from '@testing-library/react';
+import App from './App';
 
-test("app renders", () => {
-  const testId = "test-editor";
-  render(<App data-testid={testId} />);
-  const editorInstance = screen.getByTestId(testId);
-  expect(editorInstance).toBeInTheDocument();
+test('app renders', () => {
+	const testId = 'test-editor';
+	render(<App data-testid={testId} />);
+	const editorInstance = screen.getByTestId(testId);
+	expect(editorInstance).toBeInTheDocument();
 });
